3630961c57d00bd1cf7223cc9104bb94740c185d
Thalles Gabriel Dias Teixeira <thallesgabrieldias16@gmail.com>
2024-03-29 10:38:37 -0300

    new file:   .vscode/launch.json 	new file:   tipos-variaveis/src/Operadores.java 	modified:   tipos-variaveis/src/tiposVariaveis.java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+
+    int a, b;
+
+      a = 5;
+      b = 6;
+
+      //if(a==b)
+            //resultado = "verdadeiro";
+
+     // else 
+       // resultado = "Falso";
+    
+       //true or false.
+
+      String resultado = a==b ?"verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+    }
+}
